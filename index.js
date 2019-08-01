5e24a4985f0a9fae3bcc9d732f614de3ae42cf45
Denis Farrer <denis.farrer@gmail.com>
2019-08-01 22:21:46 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,43 @@
+var a, b
+
+function add(a, b) {
+  return a += b;
+}
+
+function subtract(a, b) {
+  return a -= b;
+}
+
+function multiply(a, b) {
+  return a *= b;
+}
+function divide(a, b) {
+  return a /= b;
+}
+function inc(n) {
+  n++
+  return n;
+}
+function dec(n) {
+  n--
+  return n
+}
+function makeInt(n) {
+  parseInt(n)
+ return n 
+}
+
+function makeInt(n) {
+  parseInt(n, 10)
+  return n
+}
+
+function preserveDecimal(n) {
+  parseFloat(2.222, 10)
+  return n
+}
+
+function preserveDecimal(n) {
+  parseFloat(n)
+  return n
+}
